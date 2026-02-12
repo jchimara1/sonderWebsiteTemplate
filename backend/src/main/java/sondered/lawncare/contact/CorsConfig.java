3701a1b32f4cbb0521a32a5a3ac683196bc4e673
justice chimara <jchimara1@gmail.com>
2026-02-12 14:53:26 -0600

    fixing production url

--- backend/src/main/java/sondered/lawncare/contact/CorsConfig.java
+++ backend/src/main/java/sondered/lawncare/contact/CorsConfig.java
@@ -13,7 +13,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://sonderwebsitetemplate.onrender.com"
+                        )
                         .allowedMethods("POST", "OPTIONS")
                         .allowedHeaders("*");
             }
